987d9aff135e92c51814c52aba252beba7cf159d
kkubicki <krzysiek.kubicki@gmail.com>
2020-06-19 10:11:30 +0200

    Add commentary

--- pingtest.sh
+++ pingtest.sh
@@ -9,12 +9,14 @@ do
 	tries=$((tries+1))
 done
 
+# remember communication breakdown
 date >> /mnt/share/restarts.txt
 /etc/init.d/vnstat_backup backup && sleep 1 && umount /mnt/share/
 
+# sometimes router reboot only is not enough - randomly try to reset 3g modem as well
 r=$((0x$(cut -c1-1 /proc/sys/kernel/random/uuid) % 2))
 if [ $r -eq 1 ]; then
  sleep 70 && touch /etc/banner && reboot
 else
- sleep 5 && echo -e 'AT^RESET\r' > /dev/ttyUSB2
+ sleep 1 && echo -e 'AT^RESET\r' > /dev/ttyUSB2
 fi
