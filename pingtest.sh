54b16d82e88e00d7a8f479449fd56333af84c4d7
kkubicki <krzysiek.kubicki@gmail.com>
2020-12-18 16:41:52 +0100

    Change pingtest: double check access & store network load

--- pingtest.sh
+++ pingtest.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
-tries=0
-while [[ $tries -lt 5 ]]
-do
-	if /bin/ping -c1 -w10 8.8.8.8 > /dev/null
-	then
-		exit 0
-	fi
-	tries=$((tries+1))
-done
-
-date >> /mnt/share/restarts.txt
-/etc/init.d/vnstat_backup backup && sleep 1 && umount /mnt/share/
-
-r=$((0x$(cut -c1-1 /proc/sys/kernel/random/uuid) % 2))
-if [ $r -eq 1 ]; then
- sleep 70 && touch /etc/banner && reboot
-else
- sleep 5 && echo -e 'AT^RESET\r' > /dev/ttyUSB2
+if ping -q -c 2 -W 10 8.8.8.8 > /dev/null; then
+	exit 0
 fi
+sleep 70
+if ping -q -c 2 -W 10 8.8.8.8 > /dev/null; then
+	exit 0
+fi
+touch /etc/banner
+iftop -iwlan0 -t -s 10 -L 10 > "/mnt/share/restart_$(date +%Y_%m_%d_%H_%M).log"
+/etc/init.d/vnstat_backup backup
+#echo -e 'AT^RESET\r' > /dev/ttyUSB2 
+#sleep 1
+sync
+reboot
