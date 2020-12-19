f5c94569f90612b53a411cac7e5395f1781e6b01
kkubicki <krzysiek.kubicki@gmail.com>
2020-12-19 17:07:21 +0100

    Store router status on communication breakdown

--- pingtest.sh
+++ pingtest.sh
@@ -2,14 +2,24 @@
 if ping -q -c 2 -W 10 8.8.8.8 > /dev/null; then
 	exit 0
 fi
+
+logFileName="/tmp/`date +%Y_%m_%d_%H_%M`"
+logFileNameGZ="$logFileName.gz"
+top -n1 -b | head >> $logFileName
+dmesg | tail >> $logFileName
+logread | tail >> $logFileName
+iftop -iwlan0 -t -s 10 -L 5 >> $logFileName
+gzip $logFileName
+mv $logFileNameGZ /mnt/share/
+
+/etc/init.d/vnstat_backup backup
 sleep 70
+
 if ping -q -c 2 -W 10 8.8.8.8 > /dev/null; then
 	exit 0
 fi
+
 touch /etc/banner
-iftop -iwlan0 -t -s 10 -L 10 > "/mnt/share/restart_$(date +%Y_%m_%d_%H_%M).log"
-/etc/init.d/vnstat_backup backup
-#echo -e 'AT^RESET\r' > /dev/ttyUSB2 
-#sleep 1
-sync
+echo -e 'AT^RESET\r' > /dev/ttyUSB2
+sleep 1
 reboot
